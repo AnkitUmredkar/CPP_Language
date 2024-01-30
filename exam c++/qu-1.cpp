c7984108fe56966e4d1f3581406a0436218f8e09
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-01-30 14:56:24 +0530

    Add files via upload

---
+++ exam c++/qu-1.cpp
@@ -0,0 +1,54 @@
+//1 Write a Program to create a class to read and add two distances.
+//For example,
+//Input:
+//input1 => feet: 8, inch: 16
+//input2 => feet: 4, inch: 14
+//
+//Output:
+//14 feet 6 inch
+
+#include<iostream>
+using namespace std;
+
+class Dis
+{
+	public:
+	int feet1,inc1;
+	int feet2,inc2;
+	
+	void answer()
+	{
+		cout << "Enter the feet : ";
+		cin >> feet1;
+		
+		cout << "Enter the inc : ";
+		cin >> inc1;
+		
+		cout<<endl<<endl;
+		
+		cout << "Enter the feet : ";
+		cin >> feet2;
+		
+		cout << "Enter the inc : ";
+		cin >> inc2;
+		
+		feet1 = feet1 + (inc1 / 12);
+		inc1 = (inc1 % 12);
+		
+		feet2 = feet2 + (inc2 / 12);
+		inc2 = (inc2 % 12);
+		
+		cout << endl << "Feet   : " << feet1+feet2 << endl;
+		cout << "Inches : " << inc1 + inc2;
+	}
+	
+};
+
+int main()
+{
+	class Dis a;
+	
+	a.answer();
+	
+	return 0;
+}
---
+++ exam c++/qu-2.cpp
@@ -0,0 +1,33 @@
+//2 Write a Program to create a class to read time in seconds and convert it into time in (HH:MM: SS) format.
+
+#include<iostream>
+using namespace std;
+
+class Time
+{
+	public:
+	int hh=0,mm=0,ss=0,second;
+	
+	void hms()
+	{
+		cout << "Enter the second : ";
+		cin >> second;
+		
+		hh = (second / 3600);
+		ss = (second % 3600);
+		mm = (ss / 60);
+		ss = (second % 60);
+		
+		cout << hh << " : " << mm << " : " << ss <<;
+	}
+	
+};
+
+int main()
+{
+	class Time a;
+	
+	a.hms();
+	
+	return 0;
+}

c0af62b6a02edccb10e7b4f07121dab97cf061c8
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-01-30 09:43:58 +0530

    Update qu_2.cpp

--- Class/qu_2.cpp
+++ Class/qu_2.cpp
@@ -1,32 +1,26 @@
-//Write a program to convert second into hh: mm: ss
+//3.Write a program to convert second into hh: mm: ss
 
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	int a,ss=0,mm=0,hh=0;
+	int second,ss=0,mm=0,hh=0;
 	
 	cout << "\n\nEnter the seconds :";
-	cin >> a;
+	cin >> second;
 	
-	if(a < 0)
+	if(second < 0)
 	{
 		printf("Second should be positive!!");
 	}
 	
-	ss = a % 60;
-	mm = (a / 60);
-	
-	if(mm >= 60)
-	{
-		mm = mm - 60;
-	}
-	
-	a = (a / 60);
-	hh = (a / 60);
+	hh = (second / 3600);
+	ss = (second % 3600);
+	mm = (ss / 60);
+	ss = (second % 60);
 
 	cout << hh << " : "<< mm << " : " << ss;
-	
+
  	return 0;
 }

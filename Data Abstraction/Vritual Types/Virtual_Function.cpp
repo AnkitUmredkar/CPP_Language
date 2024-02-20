bc636614e2db2d10b1995eacd1d68a834de1bb7c
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-02-20 16:12:33 +0530

    Add files via upload

---
+++ Data Abstraction/Vritual Types/Virtual_Function.cpp
@@ -0,0 +1,32 @@
+//Demonstrate of Virtual Function
+
+#include<iostream>
+using namespace std;
+
+class A
+{
+	public :
+		virtual void print()
+		{
+			cout << "Class A" << endl;	
+		}	
+};
+class B : public A
+{
+	public :
+		void print()
+		{
+			cout << "Class B";	
+		}
+};
+
+int main()
+{
+	A a1;
+	B b1;
+	
+	A *ptr = &b1;
+	ptr -> print();
+	
+	return 0;
+}

641c15eb418f93e32b1e00107fa74d520817833b
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-02-20 10:00:59 +0530

    Add files via upload

---
+++ Question Paper/Paper 2/qu_5_operator_over.cpp
@@ -0,0 +1,17 @@
+//5. Overload the `+` operator for the `BankAccount` class to display account details. Create two `BankAccount` objects and demonstrate the use of 
+//the overloaded `+` operator to display total amount of both accounts.
+
+#include<iostream>
+using namespace std;
+
+class BankAccount
+{
+		
+};
+
+int main()
+{
+
+	return 0;
+}
+
